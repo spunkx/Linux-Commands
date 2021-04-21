290f6e355ce4c85303c465e18cf348471e4f36e6
spunkx <44605890+spunkx@users.noreply.github.com>
2021-04-22 01:22:43 +0800

    Update LoonuxCommands.sh

--- LoonuxCommands.sh
+++ LoonuxCommands.sh
@@ -1,3 +1,3 @@
-sudo docker exec ($CONTAINER_NAME) bash -c "echo 'SELECT * FROM $(TABLE_NAME);' | psql -U postgres -d ($DB_NAME)"
+sudo docker exec ($CONTAINER_NAME) bash -c "echo 'SELECT * FROM TABLE_NAME;' | psql -U postgres -d ($DB_NAME)"
 #connects into running docker container, runs a bash command to run psql, connect to specific db and run a db query
 #-f (file) for postgres can be used to input lots of queries
